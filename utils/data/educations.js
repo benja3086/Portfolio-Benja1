22cb7e10f9e9e3ba63bcff155ca09f0ff73dc041
benja3086 <benjaminquinteros785@gmail.com>
2026-03-12 18:24:32 -0300

    modificado el diseño

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,13 +3,8 @@ export const educations = [
     id: 1,
     title: "UNIVERSIDAD",
     duration: "2024 - actualidad",
-    institution: "UNSTA",
+    institution: "UNSTA - Desarrollo de Software",
   },
-  {
-    id: 2,
-    title: "Raul scalbrini ortiz",
-    duration: "egresado en 2022",
-    institution: "Raul scalbrini ortiz",
-  },
+ 
   
 ]
